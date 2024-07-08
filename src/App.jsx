b643276b776477c78d68b8e6ce4d279e4f14e0d2
Edward <edward.azarevich@gmail.com>
2024-07-08 13:56:35 +0300

    add useRef component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from 'react'
+import './App.css';
+import TestUseRef from "./components/TestUseRef";
+import TestFile from './components/TestFile';
+
 
 function App() {
-    const [count, setCount] = useState(0)
-
+    const inputEl = useRef(null);
+    const onButtonClick = () => {
+        // `current` points to the mounted text input element
+        inputEl.current.focus();
+    };
     return (
-            <Promo />
-    )
-}
+        <>
+            <TestFile />
+            <TestUseRef />
+        </>
+    );
+};
 
 export default App
