f21a7d998a2d17345e94f6a31c06dfebdaf4ef1c
Edward <edward.azarevich@gmail.com>
2024-07-19 10:18:19 +0300

    add simple calc

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,65 @@
-import React, { useState, useEffect } from "react";
-import TopDrawerComponent from './components/animation/MoveComponent.jsx';
-import './index.css';
-import ReactPlayer from "react-player/youtube";
-
-
+import { useState, useRef } from "react";
+import "./App.css";
 
 function App() {
+    const inputRef = useRef(null);
+    const resultRef = useRef(null);
+    const [result, setResult] = useState(0);
+
+    function plus(e) {
+        e.preventDefault();
+        setResult((result) => result + Number(inputRef.current.value));
+    };
+
+    function minus(e) {
+        e.preventDefault();
+        setResult((result) => result - Number(inputRef.current.value));
+    };
+
+    function times(e) {
+        e.preventDefault();
+        setResult((result) => result * Number(inputRef.current.value));
+    };
+
+    function divide(e) {
+        e.preventDefault();
+        setResult((result) => result / Number(inputRef.current.value));
+    };
+
+    function resetInput(e) {
+        e.preventDefault();
+        inputRef.current.value = "";
+    };
+
+    function resetResult(e) {
+        e.preventDefault();
+        setResult(0);
+    };
+
     return (
-        <>
-            <TopDrawerComponent />
-            <MyVideo />
-        </>
+        <div className="App">
+            <div>
+                <h1>Simplest Working Calculator</h1>
+            </div>
+            <form>
+                <p ref={resultRef}>
+                    {result}
+                </p>
+                <input
+                    pattern="[0-9]"
+                    ref={inputRef}
+                    type="number"
+                    placeholder="Type a number"
+                />
+                <button onClick={plus}>add</button>
+                <button onClick={minus}>minus</button>
+                <button onClick={times}>times</button>
+                <button onClick={divide}>divide</button>
+                <button onClick={resetInput}>reset input</button>
+                <button onClick={resetResult}>reset result</button>
+            </form>
+        </div>
     );
 }
 
-const MyVideo = () => {
-    return (
-        <ReactPlayer url='https://www.youtube.com/watch?v=zMf_xeGPn6s&ab_channel=Remix' />
-    );
-};
-
-export default App
+export default App; 
